a946344578035e370ec8f699ff782f509a81bd5b
Dan Rocha <drochag1@gmail.com>
2018-05-28 09:31:43 -0500

    Trying out triangles canvas

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,42 +4,15 @@ import Triangles from 'triangles-canvas'
 
 class IndexPage extends Component {
   componentDidMount() {
-    let redraw;
-    (redraw = function () {
-      let currentWidth = 0, currentHeight = 0;
-      if( typeof( window.innerWidth ) == 'number' ) {
-        //Non-IE
-        currentWidth = window.innerWidth;
-        currentHeight = window.innerHeight;
-      } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-        //IE 6+ in 'standards compliant mode'
-        currentWidth = document.documentElement.clientWidth;
-        currentHeight = document.documentElement.clientHeight;
-      } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-        //IE 4 compatible
-        currentWidth = document.body.clientWidth;
-        currentHeight = document.body.clientHeight;
-      }
-
-      currentWidth += 1;
-      currentHeight += 1;
-
-      var container = document.getElementById('test')
-      container.setAttribute('style', `width: ${currentWidth}px; height:${currentHeight}px;`);
-    })();
-
-    if (window.attachEvent) {
-      window.attachEvent('onresize', redraw);
-    } else if (window.addEventListener) {
-      window.addEventListener('resize', redraw, true);
-    }
-
-    Triangles({ id: 'test' });
+    Triangles({ id: 'triangles' });
   }
+
   render() {
     return <div>
       <p>An npm module to have a background with animated triangles using canvas.</p>
-      <div id="test" />
+      <div id="container" style={{width: 500, height: 200}}>
+        <canvas id="triangles" />
+      </div>
     </div>
   }
 }
